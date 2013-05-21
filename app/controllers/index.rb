79a0498634e5d00bea0540f54def9403dd80c93f
Apprentice <apprentice@dbc01.local>
2013-05-20 20:18:01 -0700

    trying to figure out ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,10 @@
 get '/' do
   # Look in app/views/index.erb
-  erb :racer
+  erb :index
+end
+
+post '/create_game' do
+  Player.find_or_create_by_name(params[:username1])
+  Player.find_or_create_by_name(params[:username2])
+  redirect '/'
 end
--- public/js/racer.js
+++ public/js/racer.js
@@ -1,18 +1,31 @@
 $(document).ready(function() {
-  $(document).on('keydown', function(e) { 
-    if ($('td.last1').hasClass('active')) {
-      alert("Player 1 wins!");
-    } else if  
-      ($('td.last2').hasClass('active')) {
-      alert("Player 2 wins!");
-    } else {
-      if (e.keyCode == 65) {
-        var position1 = $('#player1_strip td.active');
-        position1.removeClass('active').next().addClass('active');
-      } else if (e.keyCode == 222){
-        var position2 = $('#player2_strip td.active');
-        position2.removeClass('active').next().addClass('active');
-      }
-    }
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+  $.ajax({
+    url: '/create_game',
+    type: 'POST',
+    data: $(this).serialize()
+  }).done(function() {
+    $('#signin').remove();
   });
 });
+
+
+
+
+// $(document).on('keydown', function(e) { 
+//     if ($('td.last1').hasClass('active')) {
+//       alert("Player 1 wins!");
+//     } else if  
+//       ($('td.last2').hasClass('active')) {
+//       alert("Player 2 wins!");
+//     } else {
+//       if (e.keyCode == 65) {
+//         var position1 = $('#player1_strip td.active');
+//         position1.removeClass('active').next().addClass('active');
+//       } else if (e.keyCode == 222){
+//         var position2 = $('#player2_strip td.active');
+//         position2.removeClass('active').next().addClass('active');
+//       }
+//     }
+//   });
